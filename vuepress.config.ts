10133a7164fcc1753aae5471740cebe387b985ad
Kariko Lin <caclx@outlook.com>
2024-11-07 04:06:25 +0800

    force using Noto Serif.

--- vuepress.config.ts
+++ vuepress.config.ts
@@ -17,4 +17,20 @@ export default defineUserConfig({
 
   // 和 PWA 一起启用
   shouldPrefetch: false,
+
+  head: [
+    // 导入相应链接
+    ["link", { rel: "preconnect", href: "https://fonts.googleapis.com" }],
+    [
+      "link",
+      { rel: "preconnect", href: "https://fonts.gstatic.com", crossorigin: "" },
+    ],
+    [
+      "link",
+      {
+        href: "https://fonts.googleapis.com/css2?family=Noto+Serif+SC:wght@200..900&display=swap",
+        rel: "stylesheet",
+      },
+    ],
+  ],
 });
